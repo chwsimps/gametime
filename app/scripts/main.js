b2b27786381b487d01175f0b38a7811a446712a3
Chase Simpson <chase.w.simpson1@gmail.com>
2014-10-26 21:18:20 -0400

    gifs added to good guy section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,22 +1,34 @@
+//Variables
+var good_guy,
+    bad_dude;
+
+//Selecting Fighters
+$('.modal-pick').on('click', function (event) {
+  event.preventDefault();
+
+  $('.btn, .modal').hide();
+});
+
 // Players being picked - GOODY GUYS
 $('.player_picked1').on('click', function (event) {
   event.preventDefault();
-  $('.good_large1').fadeIn();
-  $('.player_picked1').fadeTo(200,0.5);
+  $('.good_large1, .par_large1').fadeIn();
+  // $('.player_picked1').fadeTo(200,0.5);
+  $('.ninja_gif').fadeIn();
 
 });
 
 $('.player_picked2').on('click', function (event) {
   event.preventDefault();
   $('.good_large2').fadeIn();
-  $('.player_picked2').fadeTo(200,0.5);
+  $('.karate_gif').fadeIn();
 
 });
 
 $('.player_picked3').on('click', function (event) {
   event.preventDefault();
   $('.good_large3').fadeIn();
-  $('.player_picked3').fadeTo(200,0.5);
+  $('.surf_gif').fadeIn();
 
 });
 
@@ -49,10 +61,10 @@ var Baddude = function (name) {
   this.name = name;
   this.health = 100;
   this.attack = function (hurt) {
-    return hurt.health = hurt.health - _.random(1, 10);
+    return hurt.health = hurt.health - _.random(1, 15);
   };
   this.special = function (hurt) {
-    return hurt.health = hurt.health - _.random(10, 30);
+    return hurt.health = hurt.health - _.random(15, 30);
   };
 };
 
@@ -62,26 +74,26 @@ var Goodguy = function (options) {
   this.name = options.name;
   this.health = 100;
   this.attack = function (hurt) {
-    return hurt.health = hurt.health - _.random(1, 10);
+    return hurt.health = hurt.health - _.random(1, 15);
   };
   this.special = function (hurt) {
-    return hurt.health = hurt.health - _.random(10, 30);
+    return hurt.health = hurt.health - _.random(15, 30);
   };
 };
 
 
-var good_guy = new Goodguy({
+good_guy = new Goodguy({
     name: $(this).text(),
     type: $(this).attr('name')
   });
 
-var bad_dude = new Baddude({
+bad_dude = new Baddude({
     name: $(this).text(),
   });
 
 //Update variable to be on top of page and add switch statement to include above variables.
-      $('.goody').prepend(good_guy.name).find('.clickgood').text(good_guy.health);
-    $('.baddie').prepend(bad_dude.name).find('.clickbad').text(bad_dude.health);
+    //   $('.goody').prepend(good_guy.name).find('.clickgood').text(good_guy.health);
+    // $('.baddie').prepend(bad_dude.name).find('.clickbad').text(bad_dude.health);
 
 
 $('.choppin').on('click', function (event) {
@@ -94,7 +106,7 @@ $('.choppin').on('click', function (event) {
   $('.clickgood').text(good_guy.health);
 
 
-  if(good_guy.health >= 0) {
+  if(good_guy.health > 0) {
       $('.clickgood').text(good_guy.health);
     } else {
       $('.clickgood').text('You Be Dead!');
@@ -102,18 +114,21 @@ $('.choppin').on('click', function (event) {
       $('.choppin').hide();
     };
 
-    if(bad_dude.health >= 0) {
+  if(bad_dude.health > 0) {
         $('.clickbad').text(bad_dude.health);
-      } else {
-        $('.clickbad').text('You Be Dead!');
-        // $('.bgName').css('text-decoration', 'line-through').css('color', 'red');
-        $('.choppin').hide();
-      };
-
+    } else {
+      $('.clickbad').text('You Be Dead!');
+      // $('.bgName').css('text-decoration', 'line-through').css('color', 'red');
+      $('.choppin').hide();
+    };
 });
+
+//Shake effect for button & titles
 
 $('.choppin').click( function () {
   $('.choppin').toggleClass('animated shake');
+  $('.clickgood').toggleClass('animated shake');
+  $('.clickbad').toggleClass('animated shake');
 });
 
 // }());
